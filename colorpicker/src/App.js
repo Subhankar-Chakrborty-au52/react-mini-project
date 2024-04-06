597f480ab8084fb034d52480b9b50f5bf90718d8
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-04-06 12:44:36 +0530

    project done

---
+++ colorpicker/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function ColorPicker() {
+  const [color, setColor] = useState("#ffffff");
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Color Picker</h2>
+      <input type="color" value={color} onChange={handleChange} />
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: color,
+          margin: "20px 0",
+        }}
+      ></div>
+      <p>Selected Color: {color}</p>
+    </div>
+  );
+}
